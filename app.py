8dbf4f54ce033fe5e3d4861f6728b26e41ce50b4
ameliablack <72445369+ameliablack@users.noreply.github.com>
2021-04-12 20:51:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,11 @@ def template():
 def index():
     return render_template("index.html") 
 
+@app.route("/hobby")
+def index():
+    return render_template("hobby.html") 
+
+
+
 if __name__ =='__main__':
     app.run(debug=True)
